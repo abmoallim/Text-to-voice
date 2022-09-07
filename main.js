a1169a2d68b96af4b43f72c45adf2dd7d016f535
Amoalim4 <ahmmoallim@gmail.com>
2022-09-07 21:30:30 +0300

    pause and resume

---
+++ main.js
@@ -0,0 +1,45 @@
+$( document ).ready(function() {
+    
+
+
+
+});
+
+
+let text = document.getElementById("text")
+let play = document.getElementById("play")
+let Puse = document.getElementById("pause")
+let stop = document.getElementById("stop")
+let speed = document.getElementById("speed")
+
+play.addEventListener('click',()=>{
+    Tovoice(text.value)
+})
+pause.addEventListener('click',pauseText)
+
+stop.addEventListener('click',stopText)
+function Tovoice(txt){
+    if(speechSynthesis.pause && speechSynthesis.speaking){
+        return speechSynthesis.resume()
+    }
+    const voice = new SpeechSynthesisUtterance(text.value)
+    voice.rate = speed.value || 1
+    voice.addEventListener('end',()=>{
+        text.disabled = false
+       
+
+    })
+    text.disabled = true
+    
+    
+    speechSynthesis.speak(voice)
+}
+
+function pauseText(){
+    if(speechSynthesis.speaking)speechSynthesis.pause()
+}
+
+function stopText(){
+    speechSynthesis.resume()
+    speechSynthesis.cancel()
+}
